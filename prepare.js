1878d5ebfa1eb0bc513600df1653a4721b38bde3
MeetPatel3366 <meet3366patel@gmail.com>
2026-02-01 20:03:25 +0530

    feat: Chunk the document

--- prepare.js
+++ prepare.js
@@ -1,8 +1,16 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
   const doc = await loader.load();
 
-  console.log(doc[0].pageContent);
+  console.log("doc : ", doc[0].pageContent);
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+  const texts = await splitter.splitText(doc[0].pageContent);
+  console.log("texts : ", texts);
 }
