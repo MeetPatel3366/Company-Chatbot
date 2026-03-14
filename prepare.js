0a2065a1132df2a4fe9e60ab677dc07eb2c25a13
MeetPatel3366 <meet3366patel@gmail.com>
2026-03-14 17:34:21 +0530

    feat:  Generate vector embeddings and Indexing

--- prepare.js
+++ prepare.js
@@ -1,5 +1,21 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+const embeddings = new OpenAIEmbeddings({
+  model: "text-embedding-3-small",
+});
+
+const pinecone = new PineconeClient();
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+export const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
 
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
@@ -13,4 +29,14 @@ export async function indexTheDocument(filePath) {
   });
   const texts = await splitter.splitText(doc[0].pageContent);
   console.log("texts : ", texts);
+
+  const documents = texts.map((chunk) => {
+    return {
+      pageContent: chunk,
+      metadata: doc[0].metadata,
+    };
+  });
+
+  console.log("documents : ", documents);
+  await vectorStore.addDocuments(documents);
 }
