2b5aa6daface7e5410915353d17811b41a9afc77
MeetPatel3366 <meet3366patel@gmail.com>
2026-03-15 12:10:43 +0530

    feat: Add retrieval step & Pass input + relevant information to LLM

--- chat.js
+++ chat.js
@@ -1,16 +1,55 @@
+import readline from "node:readline/promises";
 import Groq from "groq-sdk";
+import { vectorStore } from "./prepare.js";
+
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export async function chat() {
-  const chatCompletion = await groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: "Explain the importance of fast language models",
-      },
-    ],
-    model: "openai/gpt-oss-20b",
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  console.log(chatCompletion.choices[0]?.message?.content || "");
+  while (true) {
+    const question = await rl.question("You: ");
+
+    if (question === "bye") {
+      break;
+    }
+
+    // retrieval
+    const relevantChunks = await vectorStore.similaritySearch(question, 3);
+    console.log("relevantChunks: ", relevantChunks);
+
+    const context = relevantChunks
+      .map((chunk) => chunk.pageContent)
+      .join("\n\n");
+    console.log("context: ", context);
+
+    const SYSTEM_PROMPT = `You are an assistant for question-asnwering tasks. Use the follwing relevant pieces of retrieved context to answer the question. If you don't know the answer, say I don't know`;
+
+    const userQuery = `Question: ${question}
+    Relevant context: ${context}
+    Answer:`;
+
+    const chatCompletion = await groq.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: SYSTEM_PROMPT,
+        },
+        {
+          role: "user",
+          content: userQuery
+        },
+      ],
+      model: "openai/gpt-oss-20b",
+    });
+
+    console.log(`Assistant: `,chatCompletion.choices[0]?.message?.content || "");
+  }
+
+  rl.close();
 }
+
+chat();
