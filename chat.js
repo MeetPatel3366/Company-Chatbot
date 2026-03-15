e59338eab957bd7f08a79326fe78b836b97e6893
MeetPatel3366 <meet3366patel@gmail.com>
2026-03-15 14:36:19 +0530

    feat: Enhance chat function with conversation summarization and limit message history

--- chat.js
+++ chat.js
@@ -6,12 +6,31 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 const SYSTEM_PROMPT = `You are an assistant for question-asnwering tasks. Use the follwing relevant pieces of retrieved context to answer the question. If you don't know the answer, say I don't know`;
 
-const messages = [
-  {
-    role: "system",
-    content: SYSTEM_PROMPT,
-  },
-];
+const messages = [];
+
+let conversationSummary = "";
+
+async function summarizeConversation(oldMessages) {
+  const text = oldMessages
+    .map((msg) => `${msg.role}: ${msg.content}`)
+    .join("\n");
+
+  const response = await groq.chat.completions.create({
+    messages: [
+      { role: "system", content: "You summarize conversations." },
+      {
+        role: "user",
+        content: `
+        Summarize the following conversation briefly while keeping important facts.
+        
+        Conversation:${text}`,
+      },
+    ],
+    model: "openai/gpt-oss-20b",
+  });
+
+  return response.choices[0]?.message?.content || "";
+}
 
 export async function chat() {
   const rl = readline.createInterface({
@@ -41,6 +60,14 @@ export async function chat() {
 
     const chatCompletion = await groq.chat.completions.create({
       messages: [
+        {
+          role: "system",
+          content: SYSTEM_PROMPT,
+        },
+        {
+          role: "system",
+          content: `Conversation Summary: ${conversationSummary}`,
+        },
         ...messages,
         {
           role: "user",
@@ -64,7 +91,13 @@ export async function chat() {
     });
 
     if (messages.length > 20) {
-      messages.splice(1, 2);
+      const oldMessages = messages.slice(1, 11);
+
+      const summary = await summarizeConversation(oldMessages);
+
+      conversationSummary += "\n" + summary;
+
+      messages.splice(1, 10);
     }
   }
 
